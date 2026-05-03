9a042fd8352a13887fe7cfe79c5f1b7083a4ddce
shaboy20 <138219481+shaboy20@users.noreply.github.com>
2026-05-03 08:12:03 +0530

    Update books.svelte
    =D

--- src/lib/books.svelte
+++ src/lib/books.svelte
@@ -3,46 +3,14 @@
     {
       title: "Bio 101",
       author: "John Doe",
+      details: false,
       cover:
         "https://i.pinimg.com/736x/1e/08/a2/1e08a2bd6acf19fd9530717d84a4a86c.jpg",
       summary: "An introduction to biology for beginners.",
-    },
-    {
-      title: "Bio 101",
-      author: "John Doe",
-      cover:
-        "https://i.pinimg.com/736x/1e/08/a2/1e08a2bd6acf19fd9530717d84a4a86c.jpg",
-      summary: "An introduction to biology for beginners.",
-    },
-    {
-      title: "Bio 101",
-      author: "John Doe",
-      cover:
-        "https://i.pinimg.com/736x/1e/08/a2/1e08a2bd6acf19fd9530717d84a4a86c.jpg",
-      summary: "An introduction to biology for beginners.",
-    },
-    {
-      title: "Bio 101",
-      author: "John Doe",
-      cover:
-        "https://i.pinimg.com/736x/1e/08/a2/1e08a2bd6acf19fd9530717d84a4a86c.jpg",
-      summary: "An introduction to biology for beginners.",
-    },
-
-    {
-      title: "Bio 101",
-      author: "John Doe",
-      cover:
-        "https://i.pinimg.com/736x/1e/08/a2/1e08a2bd6acf19fd9530717d84a4a86c.jpg",
-      summary: "An introduction to biology for beginners.",
-    },
-
-    {
-      title: "Bio 101",
-      author: "John Doe",
-      cover:
-        "https://i.pinimg.com/736x/1e/08/a2/1e08a2bd6acf19fd9530717d84a4a86c.jpg",
-      summary: "An introduction to biology for beginners.",
+      bids: [
+        { bidder: "Alice", amount: 10 },
+        { bidder: "Bob", amount: 15 },
+      ],
     },
   ];
 </script>
@@ -54,11 +22,41 @@
       <h2>{book.title}</h2>
       <p class="author"><strong>Author:</strong> {book.author}</p>
       <p>{book.summary}</p>
+      <button
+        on:click={() =>
+          book.details == !book.details
+            ? (book.details = book.details = true)
+            : (book.details = false)}><h2>details</h2></button
+      >
+      {#if book.details === true}
+        <div class="bids">
+          <h3>Bids:</h3>
+          {#each book.bids as bid}
+            <p>
+              <strong>{bid.bidder}:</strong> ${bid.amount}
+            </p>
+          {/each}
+        </div>
+      {:else}{/if}
     </div>
   {/each}
 </div>
 
 <style>
+  button {
+    background: transparent;
+    border: none;
+    outline: none;
+    position: relative;
+    left: 20px;
+    bottom: 60px;
+    transition: border-bottom 0.3s ease;
+  }
+
+  button:hover {
+    border-bottom: 2px solid white;
+  }
+
   div {
     display: flex;
     flex-direction: row;
