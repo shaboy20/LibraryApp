66864bbf5ac14e1f271030c191873d250957b200
shaboy20 <138219481+shaboy20@users.noreply.github.com>
2026-05-04 18:22:19 +0530

    patch:Fixed home page (#10)
    works

--- src/lib/home.svelte
+++ src/lib/home.svelte
@@ -1,1 +1,30 @@
-<div>Home placeholder :D</div>
+<script>
+  import { username } from "./library";
+</script>
+
+<div>
+  <input
+    type="text"
+    id="name"
+    bind:value={$username}
+    placeholder="enter your name"
+  />
+
+  <p>hello {$username}</p>
+</div>
+
+<style>
+  input {
+    font-size: 40px;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    border-bottom: 2px solid transparent;
+    transition: border-bottom 0.3s ease;
+    font-family: "Tangerine", cursive;
+  }
+
+  input:hover {
+    border-bottom: 2px solid white;
+  }
+</style>
